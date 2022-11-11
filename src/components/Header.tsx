e3f78eb5cbbea894995341481ad4d9318a2306f7
Victoria Cheng <victoria.cheng15@gmail.com>
2022-11-11 10:41:56 -0700

    refactor: use webp instead of png and edit backdrop class

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Avatar from "./Avatar";
 import Socials from "./Socials";
-import avatar from "../assets/avatar.png";
+import avatar from "../assets/avatar.webp";
 
 function Header() {
 	const name = "Victoria Cheng";
@@ -9,7 +9,7 @@ function Header() {
 
 	return (
 		<header className="mx-auto w-11/12 max-w-2xl">
-			<div className="mb-8 flex flex-col gap-6 rounded-md bg-slate-50 bg-opacity-20 p-4 backdrop-blur-lg backdrop-filter">
+			<div className="backdrop mb-8 flex flex-col gap-6 rounded-md bg-slate-50 bg-opacity-20 p-4">
 				<Avatar src={avatar} alt={`${name} with a full orange background`} />
 				<h1 className="text-center text-3xl font-bold tracking-wide">{name}</h1>
 				<p className="text-center text-lg font-medium tracking-wide">
