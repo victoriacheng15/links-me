28ebb5b7bcee35a5f500cb429dabafa3392f9263
Victoria Cheng <victoria.cheng15@gmail.com>
2022-11-10 19:02:55 -0700

    chore: add border for the avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -7,7 +7,7 @@ interface AvatarProps {
 
 function Avatar({ src, alt }: AvatarProps) {
 	return (
-		<div className="mx-auto mb-2 w-[150px]">
+		<div className="mx-auto mb-4 w-[150px] border-4 rounded-full border-slate-200">
 			<img className="w-full" src={src} alt={alt} />
 		</div>
 	);
