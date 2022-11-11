be8d42cd63766d64f9537162f01060291bcdf446
Victoria Cheng <victoria.cheng15@gmail.com>
2022-11-10 19:03:18 -0700

    feat: create and add Socials component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Avatar from "./Avatar";
+import Socials from "./Socials";
 import avatar from "../assets/avatar.png";
 
 function Header() {
@@ -9,10 +10,9 @@ function Header() {
 	return (
 		<header className="mx-auto w-[90%] text-center">
 			<Avatar src={avatar} alt={`${name} with a full orange background`} />
-			<div>
-				<h1 className="mb-4 text-3xl font-bold tracking-wide">{name}</h1>
-				<p className="text-xl font-bold tracking-wide">{headline}</p>
-			</div>
+			<h1 className="mb-2 text-3xl font-bold tracking-wide">{name}</h1>
+			<p className="mb-8 text-xl font-medium tracking-wide">{headline}</p>
+			<Socials />
 		</header>
 	);
 }
