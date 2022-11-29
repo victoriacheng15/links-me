50a349423bc6ad1a877785d9cb76e5bf330f906d
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-11-29 12:43:23 -0700

    Chore/reduce social size (#17)
    * chore: reduce social square size
    
    * chore: update headline and add padding
    
    * style: run prettier and rome formatter

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,11 +5,12 @@ import avatar from "../../assets/avatar.webp";
 
 function Header() {
 	const name = "Victoria Cheng";
-	const headline = "Packaging Engineer to Software Developer";
+	const headline =
+		"Software Developer | TypeScript | JavaScript | React | Express | MongoDB";
 
 	return (
 		<header className="mx-auto w-11/12 max-w-2xl">
-			<div className="backdrop mb-8 flex flex-col gap-6 rounded-md bg-slate-50 bg-opacity-20 p-4">
+			<div className="backdrop mb-8 flex flex-col gap-6 rounded-md bg-slate-50 bg-opacity-20 p-6">
 				<Avatar src={avatar} alt={`${name} with a full orange background`} />
 				<h1 className="text-center text-3xl font-bold tracking-wide">{name}</h1>
 				<p className="text-center text-lg font-medium tracking-wide">
